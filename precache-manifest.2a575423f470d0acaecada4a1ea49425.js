b51cb631f046be01e792f9fdb724e26360c011ed
arseniyT <30984464+arseniyT@users.noreply.github.com>
2019-01-22 22:02:53 +0300

    Updates

---
+++ precache-manifest.2a575423f470d0acaecada4a1ea49425.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0940fdda3b596858143",
+    "url": "/hw_ts-react-redux-calculator/static/js/runtime~main.f0940fdd.js"
+  },
+  {
+    "revision": "ed865b05a38f868dcb3f",
+    "url": "/hw_ts-react-redux-calculator/static/js/main.ed865b05.chunk.js"
+  },
+  {
+    "revision": "940cc4abbd3d6170cc9f",
+    "url": "/hw_ts-react-redux-calculator/static/js/1.940cc4ab.chunk.js"
+  },
+  {
+    "revision": "ed865b05a38f868dcb3f",
+    "url": "/hw_ts-react-redux-calculator/static/css/main.dee50102.chunk.css"
+  },
+  {
+    "revision": "885037f1792b0dafe5397067ce0490b6",
+    "url": "/hw_ts-react-redux-calculator/index.html"
+  }
+];
